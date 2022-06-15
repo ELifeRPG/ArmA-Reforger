51a0d6a031fa2070d3d87da678ada35d144a094a
TheRunyx <4083112+TheRunyx@users.noreply.github.com>
2022-06-16 00:13:38 +0200

    Add sounds for lock and unlock

--- src/Scripts/Game/Features/VehicleLock/ELIFE_VehicleLockComponent.c
+++ src/Scripts/Game/Features/VehicleLock/ELIFE_VehicleLockComponent.c
@@ -12,13 +12,18 @@ class ELIFE_VehicleLockComponent : SCR_BaseLockComponent
 	[Attribute("")]
 	string m_sDebugIdentifier;
 	
+    [Attribute("", UIWidgets.ResourceNamePicker, desc: "Lock/Unlock sound project (acp)")]
+	protected ResourceName m_lockUnlockSoundProject;
+	
+	protected AudioHandle m_AudioHandle;
+	
 	[RplProp()]
 	string m_sVehicleIdentifier;
 	
 	[RplProp()]
 	bool m_bIsVehicleLocked = false;
 	
-	private IEntity m_vehicle;
+	private IEntity m_Vehicle;
 	
 	//------------------------------------------------------------------------------------------------
 	override bool IsLocked(IEntity user, BaseCompartmentSlot compartmentSlot)
@@ -44,20 +49,49 @@ class ELIFE_VehicleLockComponent : SCR_BaseLockComponent
 		m_bIsVehicleLocked = !m_bIsVehicleLocked;
 		
 		Replication.BumpMe();
+		Rpc(Rpc_SwitchLockedState_Client, m_bIsVehicleLocked);
+		Rpc_SwitchLockedState_Client(m_bIsVehicleLocked);
 	}
 	
 	//------------------------------------------------------------------------------------------------
 	[RplRpc(RplChannel.Reliable, RplRcver.Broadcast)]
-	private void Rpc_SwitchLockedState_Client()
+	private void Rpc_SwitchLockedState_Client(bool lockedState)
 	{
-		// Play sound
+		if (!AudioSystem.IsSoundPlayed(m_AudioHandle))
+			AudioSystem.TerminateSound(m_AudioHandle);
+		
+		// Play sound		
+		string eventName = "";
+		
+		if (lockedState)
+		{
+			eventName = "SOUND_VEHICLE_UNLOCK";
+		}
+		else
+		{
+			eventName = "SOUND_VEHICLE_LOCK";
+		}
+		
+		ref array<string> signalName = new array<string>;
+		ref array<float> signalValue = new array<float>;
+		vector mat[4];
+		
+		mat[3] = m_Vehicle.GetOrigin();
+		
+		m_AudioHandle = AudioSystem.PlayEvent(m_lockUnlockSoundProject, eventName, mat, signalName, signalValue);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override void EOnInit(IEntity owner)
+	{
+		m_Vehicle = owner;
+		
+		super.EOnInit(owner);
 	}
 
 	//------------------------------------------------------------------------------------------------
 	void ELIFE_VehicleLockComponent(IEntityComponentSource src, IEntity ent, IEntity parent)
 	{
-		
-		
 		if (!Replication.IsServer())
 			return;
 		
@@ -71,6 +105,8 @@ class ELIFE_VehicleLockComponent : SCR_BaseLockComponent
 	//------------------------------------------------------------------------------------------------
 	void ~ELIFE_VehicleLockComponent()
 	{
+		// Terminate sound
+		AudioSystem.TerminateSound(m_AudioHandle);
 	}
 
 }
