ae135c9adfc6a6640e2c0bd1e2c55ce5fa1d8ccb
TheRunyx <4083112+TheRunyx@users.noreply.github.com>
2022-06-22 10:59:47 +0200

    Add check if player has a key for the vehicle

--- src/Scripts/Game/Features/VehicleLock/ELIFE_VehicleLockUtils.c
+++ src/Scripts/Game/Features/VehicleLock/ELIFE_VehicleLockUtils.c
@@ -1,29 +1,24 @@
 class ELIFE_VehicleLockUtils
 {
+	private	static ref ELIFE_VehicleKeyPredicate m_VehicleKeyPredicate = new ELIFE_VehicleKeyPredicate();
+	
 	static bool HasPlayerKeyToCar(IEntity user, string vehicleIdentifier, ResourceName keyPrefab)
 	{
 		// Get inventory of user
 		auto inventoryManager = SCR_InventoryStorageManagerComponent.Cast(user.FindComponent(SCR_InventoryStorageManagerComponent));
 		
-		if (inventoryManager.GetDepositItemCountByResource(keyPrefab) == 0)
-			return false;
+		array<IEntity> foundItems = {};
 		
-		// Get inventoryItems
-		array<IEntity> inventoryItems = new array<IEntity>();
-		inventoryManager.GetItems(inventoryItems);
+		if (inventoryManager.FindItems(foundItems, m_VehicleKeyPredicate) == 0)
+			return false;
 		
 		// Loop through all items
 		
-		foreach (IEntity item : inventoryItems)
+		foreach (IEntity item : foundItems)
 		{
 			// Check if item has InventoryItemComponent
 			InventoryItemComponent itemInventoryComponent = InventoryItemComponent.Cast(item.FindComponent(InventoryItemComponent));
 			if (!itemInventoryComponent)
-				continue;
-			
-			// Check if item has ItemAttributeCollection
-			ItemAttributeCollection itemAttributes = itemInventoryComponent.GetAttributes();
-			if (!itemAttributes)
 				continue;
 			
 			// Get VehicleKeyComponent
@@ -37,3 +32,16 @@ class ELIFE_VehicleLockUtils
 		return false;
 	}
 }
+
+class ELIFE_VehicleKeyPredicate: InventorySearchPredicate
+{
+	void ELIFE_VehicleKeyPredicate()
+	{
+		QueryComponentTypes.Insert(ELIFE_VehicleKeyComponent);
+	}
+
+	override protected bool IsMatch(BaseInventoryStorageComponent storage, IEntity item, array<GenericComponent> queriedComponents, array<BaseItemAttributeData> queriedAttributes)
+	{		
+		return (ELIFE_VehicleKeyComponent.Cast(queriedComponents[0]).m_sVehicleIdentifier != "");
+	}
+};
